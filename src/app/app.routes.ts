311b97a58dab63ed75be4ad161a0716f1c8989fb
Ivan <ivojlo2002@gmail.com>
2025-07-04 01:19:50 +0200

    added about and skills section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { JournalComponent } from './journal/journal.component';
 
 export const routes: Routes = [
      { path: '', component: HomeComponent },
+     { path: 'journal', component: JournalComponent }
 ];
