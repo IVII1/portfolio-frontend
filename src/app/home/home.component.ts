f8ce01ef2e93d2113085a95cd83987e5bfaef44a
Ivan <ivojlo2002@gmail.com>
2025-07-05 13:39:29 +0200

    home page markup finish

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+messageForm = new FormGroup({
+  name: new FormControl(''),
+  email: new FormControl(''),
+  content: new FormControl('')
+})
+onSubmit(){
+  console.log(this.messageForm.value)
+}
 }
